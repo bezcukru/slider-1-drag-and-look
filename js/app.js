0f11f1e1bc525daf7a1187a70cd7f81af16c31be
Agata Kuska <kawabezcukru@onet.eu>
2020-06-30 13:25:00 +0200

    Add a function to adjust the size of imgs

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 import '../sass/style.scss';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+ const imagesContainerEl = document.querySelector('.slider__images-container');
+ const img1El = document.querySelector('.slider__image-container--first img');
+ const img2El = document.querySelector('.slider__image-container--second img');
+
+ function adjustImageSize() {
+    const imagesContainerWidth = imagesContainerEl.offsetWidth;
+    img1El.style.width = imagesContainerWidth + 'px';
+    img2El.style.width = imagesContainerWidth + 'px';
+    }
+
+window.addEventListener('resize', adjustImageSize);
+
+adjustImageSize();
+
+})
