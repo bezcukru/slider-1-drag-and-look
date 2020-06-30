b9e238db96f80f308ad26720d825ece19a7e0e35
Agata Kuska <kawabezcukru@onet.eu>
2020-06-30 16:51:12 +0200

    Add functionalities for dragging

--- js/app.js
+++ js/app.js
@@ -7,8 +7,53 @@ document.addEventListener('DOMContentLoaded', () => {
  const img1El = document.querySelector('.slider__image-container--first img');
  const img2El = document.querySelector('.slider__image-container--second img');
 
+ let dragging = false;
+ let imagesContainerLeftOffset;
+ const img1Container = document.querySelector('.slider__image-container--first');
+ const img2Container = document.querySelector('.slider__image-container--second');
+ const handleEl = document.querySelector('.slider__handle');
+ const dividerEl = document.querySelector('.slider__divider');
+ let imagesContainerWidth;
+
+ function getOffset(clientX) {
+     const offset = clientX - imagesContainerLeftOffset;
+     if (offset < 0) {
+         return 0;
+     } else if (offset > imagesContainerWidth){
+        return imagesContainerWidth;
+     } else {
+         return offset;
+     }
+ }
+
+ function move(clientX) {
+     const offset = getOffset(clientX);
+     const percentage = offset / imagesContainerWidth * 100;
+     dividerEl.style.left = percentage + '%';
+     img2Container.style.width = percentage + '%';
+ }
+
+ function initEvents() {
+    handleEl.addEventListener('mousedown', () => {
+        dragging = true;
+    });
+
+    handleEl.addEventListener('mouseup', () => {
+        dragging = false;
+    });
+
+    window.addEventListener('mousemove', event => {
+        if (dragging) {
+            move(event.clientX);
+        }
+    });
+
+}
+
+
  function adjustImageSize() {
-    const imagesContainerWidth = imagesContainerEl.offsetWidth;
+    imagesContainerWidth = imagesContainerEl.offsetWidth;
+    imagesContainerLeftOffset = imagesContainerEl.offsetLeft;
     img1El.style.width = imagesContainerWidth + 'px';
     img2El.style.width = imagesContainerWidth + 'px';
     }
@@ -16,5 +61,6 @@ document.addEventListener('DOMContentLoaded', () => {
 window.addEventListener('resize', adjustImageSize);
 
 adjustImageSize();
+initEvents();
 
 })
